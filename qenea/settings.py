b1698e6387ce8128123629d1d84f8981dcc6355d
Seth250 <ayowaleakintayo@gmail.com>
2020-09-21 18:35:42 +0100

    added custom user

--- qenea/settings.py
+++ qenea/settings.py
@@ -101,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
--- questans/models.py
+++ questans/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Question(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='questions',
+                             related_query_name='question')
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    upvotes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='question_upvotes')
+    downvotes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='question_downvotes')
