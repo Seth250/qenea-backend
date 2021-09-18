cbe41235a689f8ed7b753cbe4aa6af1b95531ba9
Seth250 <ayowaleakintayo@gmail.com>
2021-09-18 23:05:26 +0100

    fix create superuser bug

--- accounts/managers.py
+++ accounts/managers.py
@@ -23,10 +23,10 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        if not extra_fields.get('is_staff') is not True:
+        if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
 
-        if not extra_fields.get('is_superuser') is not True:
+        if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
         return self._create_user(email, password, **extra_fields)
