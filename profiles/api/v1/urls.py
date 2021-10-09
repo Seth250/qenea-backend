e2709b52e139acc3257d776f7625af6b80a906a6
seth250 <ayowaleakintayo@gmail.com>
2021-10-09 08:56:42 +0100

    update profiles url names

--- profiles/api/v1/urls.py
+++ profiles/api/v1/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
 
+from . import views
 
 app_name = 'Profiles_API_v1'
 
 urlpatterns = [
-    path('profiles/me/', views.ProfileRetrieveUpdateAPIView.as_view()),
-    path('profiles/<str:username>/', views.ProfileDetailAPIView.as_view())
+    path('profiles/me/', views.ProfileRetrieveUpdateAPIView.as_view(), name='user-profile'),
+    path('profiles/<str:username>/', views.ProfileDetailAPIView.as_view(), name='profile-detail')
 ]
