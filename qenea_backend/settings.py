94821a9eb1c9a455c7aaea73d77fb9f8c29ccc73
seth250 <ayowaleakintayo@gmail.com>
2021-10-09 23:25:01 +0100

    add frontend url env var to settings

--- qenea_backend/settings.py
+++ qenea_backend/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from decouple import config
 from pathlib import Path
+
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -175,3 +176,7 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 
 CELERY_TASK_SERIALIZER = 'json'
+
+
+# FRONTEND URLS
+BASE_FRONTEND_URL = config('BASE_FRONTEND_URL')
