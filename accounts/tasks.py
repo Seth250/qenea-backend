a24f96807cbcf6502eb62f87c9ab5b4c4790c581
seth250 <ayowaleakintayo@gmail.com>
2021-10-16 12:52:56 +0100

    add error handling to email task

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,8 +1,12 @@
+import logging
+import smtplib
 from typing import Optional
 
 from django.core.mail import EmailMultiAlternatives
 
 from celery import shared_task
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task(name='send email to user')
@@ -11,4 +15,7 @@ def send_email(subject: str, body: str, to: str, fail_silently: bool = False, ht
     if html_content:
         email.attach_alternative(html_content, 'text/html')
 
-    return email.send(fail_silently=fail_silently)
+    try:
+        return email.send(fail_silently=fail_silently)
+    except smtplib.SMTPException as e:
+        logger.error(e)
