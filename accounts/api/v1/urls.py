e54f3454ca1d328798cc812f00fc7d92d35dcf10
seth250 <ayowaleakintayo@gmail.com>
2021-10-18 07:45:15 +0100

    add password reset request endpoint

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from . import views
 
+from . import views
 
 app_name = 'Accounts_API_v1'
 
 urlpatterns = [
     path('auth/users/', views.UserCreateAPIView.as_view(), name='signup'),
     path('auth/token/login/', views.ObtainAuthTokenAPIView.as_view(), name='login'),
-    path('auth/token/logout/', views.AuthTokenDestroyAPIView.as_view(), name='logout')
+    path('auth/token/logout/', views.AuthTokenDestroyAPIView.as_view(), name='logout'),
+    path('auth/password-reset/request/', views.RequestPasswordResetEmailAPIView.as_view(), name='password-reset-request')
 ]
