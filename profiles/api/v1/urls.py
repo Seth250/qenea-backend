be4b770f13b3991f8a80a1700cc54248e36321a7
seth250 <ayowaleakintayo@gmail.com>
2021-11-07 20:23:53 +0100

    alter follow-toggle route

--- profiles/api/v1/urls.py
+++ profiles/api/v1/urls.py
@@ -7,5 +7,5 @@ app_name = 'Profiles_API_v1'
 urlpatterns = [
     path('profiles/me/', views.ProfileRetrieveUpdateAPIView.as_view(), name='user-profile'),
     path('profiles/<str:username>/', views.ProfileDetailAPIView.as_view(), name='profile-detail'),
-    path('profiles/<str:username>/follow-toggle/', views.FollowToggleProfileAPIView.as_view(), name='follow-toggle')
+    path('profiles/<int:pk>/follow-toggle/', views.ProfileFollowToggleAPIView.as_view(), name='follow-toggle')
 ]
