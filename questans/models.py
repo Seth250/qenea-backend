fc24a074f03dae48b4b84d0e01a05467b4b70916
seth250 <ayowaleakintayo@gmail.com>
2021-11-09 06:22:17 +0100

    alter tag name field

--- questans/models.py
+++ questans/models.py
@@ -8,7 +8,7 @@ from django.utils.text import slugify
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.SlugField(max_length=30)
 
     def __str__(self):
         return self.name
