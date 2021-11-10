447128e31d4ac657246beac48ffe0f81d3c90d74
seth250 <ayowaleakintayo@gmail.com>
2021-11-10 19:16:41 +0100

    update question serializer

--- questans/api/v1/serializers.py
+++ questans/api/v1/serializers.py
@@ -4,8 +4,13 @@ from questans.models import Question
 
 
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='Questans_API_v1:question-detail',
+        lookup_field='slug'
+    )
+    user = serializers.StringRelatedField()
     total_points = serializers.ReadOnlyField()
 
     class Meta:
         model = Question
-        fields = ('slug', 'title', 'description', 'total_points', 'created_at', 'updated_at')
+        fields = ('url', 'user', 'slug', 'title', 'description', 'total_points', 'created_at', 'updated_at')
