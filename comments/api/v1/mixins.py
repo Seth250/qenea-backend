636689450c0243886e4e813fe26d25987623634d
seth250 <ayowaleakintayo@gmail.com>
2021-11-11 13:32:51 +0100

    add comment mixin

---
+++ comments/api/v1/mixins.py
@@ -0,0 +1,21 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from .serializers import CommentSerializer
+
+
+# mixin to add comments route to viewsets
+class ObjectCommentsViewSetMixin(object):
+
+    @action(methods=['GET'], detail=True)
+    def comments(self, request, *args, **kwargs):
+        obj = self.get_object()
+        comments = obj.comments.select_related('user')
+
+        page = self.paginate_queryset()
+        if page is not None:
+            serializer = CommentSerializer(page, many=True, context={'request': request})
+            return self.get_paginated_response(serializer.data)
+
+        serializer = CommentSerializer(comments, many=True, context={'request': request})
+        return Response(serializer.data)
