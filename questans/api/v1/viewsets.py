47748b4c5f278f422a11b33856163e94edaad7de
seth250 <ayowaleakintayo@gmail.com>
2021-11-11 14:15:08 +0100

    update question viewset and add answer viewset

--- questans/api/v1/viewsets.py
+++ questans/api/v1/viewsets.py
@@ -1,10 +1,10 @@
 from rest_framework import permissions, viewsets
 
 from comments.api.v1.mixins import ObjectCommentsViewSetMixin
-from questans.models import Question
+from questans.models import Answer, Question
 from questans.permissions import IsObjectUserOrReadOnly
 
-from .serializers import QuestionSerializer
+from .serializers import AnswerSerializer, QuestionSerializer
 
 
 class QuestionViewSet(ObjectCommentsViewSetMixin, viewsets.ModelViewSet):
@@ -39,7 +39,49 @@ class QuestionViewSet(ObjectCommentsViewSetMixin, viewsets.ModelViewSet):
 
     def destroy(self, request, *args, **kwargs):
         """
-        Endpoint that allows authenticated users to delete Questions they created
+        Endpoint that allows authenticated users to delete the Questions they created
+        """
+        return super().destroy(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class AnswerViewSet(ObjectCommentsViewSetMixin, viewsets.ModelViewSet):
+    queryset = Answer.objects.select_related('user', 'question')
+    serializer_class = AnswerSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsObjectUserOrReadOnly)
+
+    def list(self, request, *args, **kwargs):
+        """
+        Endpoint that provides users (unauthenticated or authenticated) with list action for Answer
+        """
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """
+        Endpoint that provides authenticated users with create action for Answer
+        """
+        return super().create(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Endpoint that provides users (unauthenticated or authenticated) with retrieve action for Answer
+        """
+        return super().retrieve(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        """
+        Endpoint that allows authenticated users to update the Answers they created
+        """
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        """
+        Endpoint that allows authenticated users to delete the Answers they created
         """
         return super().destroy(request, *args, **kwargs)
 
