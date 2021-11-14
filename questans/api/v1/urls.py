3525c1fba439cfac54234172cd50d7e49962df7d
seth250 <ayowaleakintayo@gmail.com>
2021-11-14 22:03:40 +0100

    add upvote and downvote toggle endpoints

--- questans/api/v1/urls.py
+++ questans/api/v1/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from . import viewsets
+from . import views, viewsets
 
 router = DefaultRouter()
 router.register('questions', viewsets.QuestionViewSet, basename='question')
@@ -11,5 +11,9 @@ router.register('answers', viewsets.AnswerViewSet, basename='answer')
 app_name = 'Questans_API_v1'
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('questions/<str:slug>/upvote-toggle/', views.QuestionUpvoteToggleAPIView.as_view(), name='question-upvote-toggle'),
+    path('questions/<str:slug>/downvote-toggle/', views.QuestionDownvoteToggleAPIView.as_view(), name='question-downvote-toggle'),
+    path('answers/<int:pk>/upvote-toggle/', views.AnswerUpvoteToggleAPIView.as_view(), name='answer-upvote-toggle'),
+    path('answers/<int:pk>/downvote-toggle/', views.AnswerDownvoteToggleAPIView.as_view(), name='answer-downvote-toggle')
 ]
