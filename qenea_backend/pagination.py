7392db6259c6dbfee21043f0f843130125b86494
seth250 <ayowaleakintayo@gmail.com>
2021-12-06 10:39:38 +0100

    alter page size query parameter

--- qenea_backend/pagination.py
+++ qenea_backend/pagination.py
@@ -3,5 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class DefaultPageNumberPagination(PageNumberPagination):
     page_size = 10
-    page_size_query_param = 'page-size'
+    page_size_query_param = 'size'
     max_page_size = 100
