b6083cc2f715805cb81893dd786bc8024e3aa030
seth250 <ayowaleakintayo@gmail.com>
2021-12-22 17:22:16 +0100

    add message constants

---
+++ accounts/messages.py
@@ -0,0 +1,6 @@
+from django.utils.translation import gettext_lazy as _
+
+
+class Messages:
+    NON_UNIQUE_USERNAME = _('Oops! This username is taken, please try another one.')
+    NON_UNIQUE_EMAIL = _('Oops! A user with email already exists.')
