8a62dd69e94093793fd746a1ebd5c954ee5d2ecd
seth250 <ayowaleakintayo@gmail.com>
2021-12-22 10:48:44 +0100

    update custom permissions

--- questans/permissions.py
+++ questans/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsObjectUserOrReadOnly(BasePermission):
     """
-    Custom permission to only allow the object's user (owner) to modify it 
+    Custom permission to only allow write access to object's user (owner) or allow read-only requests
     """
 
     def has_object_permission(self, request, view, obj):
@@ -11,4 +11,13 @@ class IsObjectUserOrReadOnly(BasePermission):
         if request.method in SAFE_METHODS:
             return True
 
+        return obj.user == request.user
+
+
+class IsObjectUser(BasePermission):
+    """
+    Custom permission to only allow access to the object's user (owner)
+    """
+
+    def has_object_permission(self, request, view, obj):
         return obj.user == request.user
