3f5bf08ed61fe0fc17b064f06404d48304018f52
seth250 <ayowaleakintayo@gmail.com>
2022-01-08 20:44:35 +0100

    add default token error message

--- accounts/messages.py
+++ accounts/messages.py
@@ -4,3 +4,4 @@ from django.utils.translation import gettext_lazy as _
 class Messages:
     NON_UNIQUE_USERNAME = _('Oops! This username is taken, please try another one.')
     NON_UNIQUE_EMAIL = _('Oops! A user with email already exists.')
+    INVALID_TOKEN = _('Invalid token, please request a new one.')
