dfa7ad12188e9f0e3d18c1940c9426ca595062a7
seth250 <ayowaleakintayo@gmail.com>
2022-12-28 16:15:25 +0100

    refactor: update email celery task

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,18 +1,24 @@
-import logging
+from __future__ import annotations
+
 import smtplib
-from typing import Optional
+from typing import Sequence
 
 from django.core.mail import EmailMultiAlternatives
 
 from celery import shared_task
+from celery.utils.log import get_task_logger
 
-logger = logging.getLogger(__name__)
+logger = get_task_logger(__name__)
 
 
 @shared_task(name='send email to user')
-def send_email(subject: str, body: str, to: str, fail_silently: bool = False, html_content: Optional[str] = None):
-    email = EmailMultiAlternatives(subject=subject, body=body, to=[to])
-    if html_content:
+def send_email(subject: str, body: str, to: str | Sequence[str], from_email: str | None = None,
+               html_content: str | None = None, fail_silently: bool = False):
+    if isinstance(to, str):
+        to = [to]
+
+    email = EmailMultiAlternatives(subject=subject, body=body, from_email=from_email, to=to)
+    if html_content is not None:
         email.attach_alternative(html_content, 'text/html')
 
     try:
