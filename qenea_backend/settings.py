8cf2501f1c9f7549e7164922097e2cae78d21107
seth250 <ayowaleakintayo@gmail.com>
2023-01-10 20:25:42 +0100

    chore: add asgi application to project settings

--- qenea_backend/settings.py
+++ qenea_backend/settings.py
@@ -90,6 +90,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'qenea_backend.wsgi.application'
 
+ASGI_APPLICATION = 'qenea_backend.asgi.application'
+
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
